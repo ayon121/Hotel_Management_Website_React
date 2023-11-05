e7498bcdce39fd73db438e6c4a57e87802f4c0ac
ayon121 <borshonsaha1234@gmail.com>
2023-11-05 14:20:20 +0600

    navbanner design completed

--- src/Components/Navbar/Navbanner.jsx
+++ src/Components/Navbar/Navbanner.jsx
@@ -1,23 +1,26 @@
 import { Link } from "react-router-dom";
-
+import { FaPhoneSquareAlt } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 const Navbanner = () => {
     return (
         <div className=" bg-yellow-400 w-full py-3 font-Hind">
             <div className="max-w-6xl mx-auto flex justify-between">
                 {/* Phone */}
-                <div>
+                <div className="flex items-center gap-2">
+                    <p className="text-[#3fb43b] text-2xl border-2 border-black rounded"><FaPhoneSquareAlt></FaPhoneSquareAlt></p>
                     <h1 className="font-semibold">Phone : +0983520483</h1>
                 </div>
                 {/* email */}
-                <div>
-                    <h1 className="font-semibold">Email : Roomvue@gmail.com </h1>
+                <div className="flex items-center gap-2">
+                    <p className="text-[#3fb43b] text-2xl border-2 border-black rounded"><MdEmail></MdEmail></p>
+                    <h1 className="font-semibold">Email : roomvueinfo@gmail.com </h1>
                 </div>
                 {/* Social Links */}
-                <div className="flex justify-between gap-3 underline">
-                    <Link><h1 className="font-semibold">Facebook</h1></Link>
-                    <Link><h1 className="font-semibold">LinkedIn</h1></Link>
-                    <Link><h1 className="font-semibold">Youtube</h1></Link>
+                <div className="flex justify-between gap-3 ">
+                    <Link><h1 className="font-semibold hover:text-[#317530] underline">Facebook</h1></Link>
+                    <Link><h1 className="font-semibold hover:text-[#317530] underline">LinkedIn</h1></Link>
+                    <Link><h1 className="font-semibold hover:text-[#317530] underline">Youtube</h1></Link>
                 </div>
             </div>
         </div>
